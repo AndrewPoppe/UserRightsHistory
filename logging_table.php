c7bf331d791ea4dab6d213ecd94797822831b105
Andrew Poppe <AndrewBPoppe@gmail.com>
2022-11-22 14:25:10 -0800

    start the logging table

--- logging_table.php
+++ logging_table.php
@@ -1,8 +1,35 @@
 <?php
 $module->showPageHeader("logging_table", $description);
+$json = json_encode(json_decode('{"post_award_administration":{"id":2,"title":"Post-Award Administration","survey_id":null}}'), JSON_PRETTY_PRINT);
+echo "<pre>";
+$logs = $module->getLogs();
+echo "</pre>";
+
 ?>
 <p>
     This page
     <br>
     ...
 </p>
+<style>
+    .string {
+        color: green;
+    }
+
+    .number {
+        color: darkorange;
+    }
+
+    .boolean {
+        color: blue;
+    }
+
+    .null {
+        color: magenta;
+    }
+
+    .key {
+        color: darkred;
+    }
+</style>
+<?php
